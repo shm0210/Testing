e8c41fd5fda55763203152326eec3bdd77487398
SHUBHAM <shubhampc7084@gmail.com>
2025-08-02 22:30:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -467,15 +467,15 @@ function initPlaylists() {
         playlists = [
             {
                 id: 1,
-                name: 'Favorites',
+                name: 'Recent Songs',
                 cover: 'https://via.placeholder.com/150',
-                songs: []
+                songs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
             },
             {
                 id: 2,
-                name: 'Recently Played',
+                name: 'Spritual',
                 cover: 'https://via.placeholder.com/150',
-                songs: []
+                songs: [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
             }
         ];
         savePlaylists();
